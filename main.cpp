ad953625943d07b51b6b767bce1cd0a3ce90fb56
Lizzie264 <57414831+Lizzie264@users.noreply.github.com>
2021-09-22 17:46:55 -0500

    Function Calculate Gravity Point Done

--- main.cpp
+++ main.cpp
@@ -9,96 +9,108 @@ using namespace std;
 /**
  @brief function used to draw circles with center found in houghcircles vec3f results
  **/
-void drawCircles(vector<Vec3f> &results, Mat &toMat) {
+void drawCircles(vector<Vec3f>& results, Mat& toMat) {
 
-    for (auto &result: results) {
-        Vec3i c = result;
-        Point center = Point(c[0], c[1]);
-        // circle center
-        circle(toMat, center, 1, Scalar(255, 0, 255), 1, LINE_AA);
-        // circle outline
-        int radius = c[2];
-        circle(toMat, center, radius, Scalar(255, 0, 255), 1, LINE_AA);
-    }
+	for (auto& result : results) {
+		Vec3i c = result;
+		Point center = Point(c[0], c[1]);
+		// circle center
+		circle(toMat, center, 1, Scalar(255, 0, 255), 1, LINE_AA);
+		// circle outline
+		int radius = c[2];
+		circle(toMat, center, radius, Scalar(255, 0, 255), 1, LINE_AA);
+	}
 }
 
-Mat clearImg(Mat &img) {
-    Mat blacked_canny, bilateral;
-    GaussianBlur(img, blacked_canny, Size(7, 7), 0, 0);
-    bilateralFilter(blacked_canny, bilateral, 9, 80, 80);
-    /*
-     * SOME TEST WITH FINDCOUNTOURS IN ORDER TO CLEAR BETTER BUT WITH NO DRAMATICALLY IMPROVEMENT
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    findContours(preprocessed, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-    drawContours(preprocessed, contours, -1, (255, 0, 255), 0.1);
-     */
+Mat clearImg(Mat& img) {
+	Mat blacked_canny, bilateral;
+	GaussianBlur(img, blacked_canny, Size(7, 7), 0, 0);
+	bilateralFilter(blacked_canny, bilateral, 9, 80, 80);
+	/*
+	 * SOME TEST WITH FINDCOUNTOURS IN ORDER TO CLEAR BETTER BUT WITH NO DRAMATICALLY IMPROVEMENT
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	findContours(preprocessed, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+	drawContours(preprocessed, contours, -1, (255, 0, 255), 0.1);
+	 */
 
-    return bilateral;
+	return bilateral;
 }
 
-void detectPupil(Mat &preprocessed, Mat &initialIMG) {
-    Mat canny;
-    Canny(preprocessed, canny, 20, 40, 3);
-    //TODO: IN CALIBRATION  STORE THE  VALUES FOR THE PUPIL OF THAT SESION
-    vector<Vec3f> storage;
-    HoughCircles(canny, storage, HOUGH_GRADIENT, 2,
-                 canny.rows / 16, 100, 80, 15, 40);
-    drawCircles(storage, initialIMG);
+void detectPupil(Mat& preprocessed, Mat& initialIMG) {
+	Mat canny;
+	Canny(preprocessed, canny, 20, 40, 3);
+	//TODO: IN CALIBRATION  STORE THE  VALUES FOR THE PUPIL OF THAT SESION
+	vector<Vec3f> storage;
+	HoughCircles(canny, storage, HOUGH_GRADIENT, 2,
+		canny.rows / 16, 100, 80, 15, 40);
+	//calcGravityPoint(storage, gp);
+	drawCircles(storage, initialIMG);
 }
 
-void detectIRLEDS(Mat &preprocessed, Mat &initialIMG) {
-    Mat canny;
-    Canny(preprocessed, canny, 150, 250, 3);
+void detectIRLEDS(Mat& preprocessed, Mat& initialIMG) {
+	Mat canny;
+	Canny(preprocessed, canny, 150, 250, 3);
 
-    vector<Vec3f> storage;
-    HoughCircles(canny, storage, HOUGH_GRADIENT, 2,
-                 5, 500, 15, 1, 5);
-    drawCircles(storage, initialIMG);
+	vector<Vec3f> storage;
+	HoughCircles(canny, storage, HOUGH_GRADIENT, 2,
+		5, 500, 15, 1, 5);
+	drawCircles(storage, initialIMG);
+}
+
+Point calcGravityPoint(Vec3f result, Point gp) {
+	//Get important coordinates
+	int xn = result[0] - result[2];
+	int yn = result[1] + result[2];
+
+	Point unitarygp((xn - gp.x) / result[2] * 2, (yn - gp.y) / result[2] * 2);
+
+	return unitarygp;
+
 }
 
 void processImage(Mat tempImg) {
-    Mat preprocessed = clearImg(tempImg);
-    detectPupil(preprocessed, tempImg);
-    detectIRLEDS(preprocessed, tempImg);
-    imshow("test", tempImg);
-    if (waitKey(200) >= 0);
+	Mat preprocessed = clearImg(tempImg);
+	detectPupil(preprocessed, tempImg);
+	detectIRLEDS(preprocessed, tempImg);
+	imshow("test", tempImg);
+	if (waitKey(200) >= 0);
 }
 
 //Opens a folder with images and stores them on the images vector
-void openTestImages(vector<Mat> &images, vector<Mat> &processedImages, const string &folderName) {
-    vector<cv::String> fn; //Vector  for saving filenames  of folder
-    string filDir = "./" + folderName + "/*.bmp";
-    cv::glob(filDir, fn, true);
-    //Iterating over the filenames
-    for (const cv::String &filename: fn) {
-        const Mat tempImg = imread(filename); //Creating the image from the filename
-        processImage(tempImg); //Processing the images
-    }
+void openTestImages(vector<Mat>& images, vector<Mat>& processedImages, const string& folderName) {
+	vector<cv::String> fn; //Vector  for saving filenames  of folder
+	string filDir = "./" + folderName + "/*.bmp";
+	cv::glob(filDir, fn, true);
+	//Iterating over the filenames
+	for (const cv::String& filename : fn) {
+		const Mat tempImg = imread(filename); //Creating the image from the filename
+		processImage(tempImg); //Processing the images
+	}
 }
 
 void openCamara() {
-    VideoCapture cap(2);
-    if (!cap.isOpened()) {
-        cout << "Camara not found";
-        return;
-    }
-    for (;;) {
-        Mat frame, resized;
-        cap >> frame;
-        if (waitKey(50) >= 0) break;
-        resize(frame, resized, Size(400, 394));
-        imshow("Test", resized);
-        cout << "Mat of rows: " << frame.rows << "Mat of columns" << frame.cols << endl;
-    }
+	VideoCapture cap(2);
+	if (!cap.isOpened()) {
+		cout << "Camara not found";
+		return;
+	}
+	for (;;) {
+		Mat frame, resized;
+		cap >> frame;
+		if (waitKey(50) >= 0) break;
+		resize(frame, resized, Size(400, 394));
+		imshow("Test", resized);
+		cout << "Mat of rows: " << frame.rows << "Mat of columns" << frame.cols << endl;
+	}
 
 }
 
 int main() {
-    vector<Mat> images;
-    vector<Mat> resultImages;
-    openTestImages(images, resultImages, "IrisDB");
-    //openCamara();
-    return EXIT_SUCCESS;
+	vector<Mat> images;
+	vector<Mat> resultImages;
+	openTestImages(images, resultImages, "IrisDB");
+	//openCamara();
+	return EXIT_SUCCESS;
 }
 
